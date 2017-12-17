ee2f842a938a30a2ecdde383e44a05e844779e6c
andrew <andrew.eldredge@gmail.com>
2017-12-17 00:23:50 -0500

    State map now picks uup states landed in midflight

--- actions/statemap.php
+++ actions/statemap.php
@@ -17,6 +17,25 @@ class actions_statemap
     $body .= "<div id=\"mapdiv\" ";
     $body .= "style=\"width: 750px; height: 450px;\"></div>\n";
 
+    ///////////////////////////////////////
+    //Query States of Midflight Landings //
+    ///////////////////////////////////////
+    $midflight = mysql_query("SELECT lc.state as state,
+                              COUNT(*) AS stLnd
+                              FROM locations AS lc 
+                              INNER JOIN flightWaypoints AS fwp
+                              ON fwp.locationID = lc.id 
+                              WHERE fwp.landing = 1
+                              GROUP BY lc.state 
+                              ORDER BY lc.state DESC",
+                              $this->app->db());
+    if(!$midflight)
+    {
+       $body .= "Query Error: Mid Flight Landings";
+       $body .= mysql_error();
+    }
+
+
     /////////////////////////////////////////
     //Query States of Destination airports //
     /////////////////////////////////////////
@@ -32,36 +51,70 @@ class actions_statemap
                            $this->app->db());
     if(!$result)
     {
-       $body .= "Query Error: States";
+       $body .= "Query Error: Destination States";
        $body .= mysql_error();
     }
     else
     {
+       //copy rows into an array
        while($row = mysql_fetch_assoc($result))  //Fetch all rows
        {
-          //append data to array building the table
-          $data[] = $row;
-          $statesJson .= "{\n \"id\":\"US-";
-          $statesJson .= $row['state'];
-          $statesJson .= "\",\n\"showAsSelected\":true\n},";
-          //$picHours[]   = array((float)$row['total_pic'],(int)0);
-          //$picHours = (float)$row['total_pic'];
+         $destinationData[] = $row;
        }
-       //Frees the result after finished using it
        mysql_free_result($result); 
-       //// Create new RecordGrid with the data, works nicely
-       $grid = new Dataface_RecordGrid($data);  
-       $body .= $grid->toHTML();   // Get the HTML of the RecordGrid
-
     }
 
+    //Merge midflight data with the destination data
+    if($midflight)
+    {
+       //Iterate the midflight data
+       while($midRow = mysql_fetch_assoc($midflight))
+       {
+          //$body .= "\n <br> Midflight state ";
+          //$body .= $midRow['state'];
+          $foundState = False;
+          foreach($destinationData as &$stateRow)
+          {
+             if($stateRow['state'] == $midRow['state'])
+             {
+                //$body .= "\n <br> &nbsp Landings before: ";
+                //$body .= $stateRow['stLnd'];
+                $foundState = True;
+                $stateRow['stLnd'] += $midRow['stLnd'];
+                //$body .= "\n <br>  &nbsp Landings after: ";
+                //$body .= $stateRow['stLnd'];
+             }
+          }
+          if(!$foundState)
+          {
+            //Add this state to the end of the list
+            //$body .= "\n <br> &nbsp State not found, appending. ";
+            array_push($destinationData,
+                       array("state" => $midRow['state'],
+                             "stFlt" => 0,
+                             "stLnd" => $midRow['stLnd']));
+          }
+       }
+    }
+    
+
+    foreach($destinationData as $combinedRow)
+    {
+          $statesJson .= "{\n \"id\":\"US-";
+          $statesJson .= $combinedRow['state'];
+          $statesJson .= "\",\n\"showAsSelected\":true\n},";
+    }
+    $grid = new Dataface_RecordGrid($destinationData);  
+    $body .= $grid->toHTML();   // Get the HTML of the RecordGrid
 
     /////////////////
     //Finishing Stuff
     /////////////////
 
     //Here are some extra notes
-    $body .= "<br/>\n Landing counts for each state may be inaccurate; Total landings for a flight are counted and added to the state of the destination";
+    $body .= "<br/>\n Landing counts for each state may be inaccurate;";
+    $body .= "Total landings for a flight are counted and added to the ";
+    $body .= "state of the destination";
 
     // Show the built-up content in a template derived from the main
     // Template
